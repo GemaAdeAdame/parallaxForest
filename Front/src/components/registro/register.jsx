3dd29e87ac460ee98f5a2bd7684cb8e2646f2463
Belen Zuma <belen.zumi@gmail.com>
2023-04-12 16:06:48 +0200

    login, registro, parallax

--- Front/src/components/registro/register.jsx
+++ Front/src/components/registro/register.jsx
@@ -1,20 +1,16 @@
 import React, {useState} from 'react'
 import axios from '../../api/axios'
-
-
-
-
+import Logo from '../images/logoAventurero.png';
+import GolondrinaL from '../images/birdLeft.png';
+import GolondrinaR from '../images/birdRight.png';
 
 const REGISTRATION_URL = '/api/register'
-
 
 function Register() {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [success, setSuccess] = useState(false)
-   
-
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -36,20 +32,27 @@ function Register() {
     }
 
   return (
-    <div>
+    
+           <div className='d-flex container justify-content-center'>
+    <div className=''>
+          <header>       
+           <img src={GolondrinaL} id="bird1" />
+           <img src={Logo} />
+          <img src={GolondrinaR} id="bird2" /> 
+      </header>
         {success ? (
                 <section className='success'>
                     <h2>¡Registro completado!</h2>
-                    <button className='btn-login' onClick={() => window.location.pathname='/Login'}>Ir al inicio de sesión</button>
-
+                    <a href='#' className='btn-login'>Ve al inicio de sesión</a>
                 </section> 
             ) : (
-                <section>
+                <section id='secRegister'>
                     <h1>Registro de usuario</h1>
 
                         <div className='box-registration'>
                             <form onSubmit={handleSubmit}>
-                                <label htmlFor='username'>username</label>
+                            <div  className="mb-3">
+                                <label htmlFor='username'>Usuario</label>
                                 <input
                                     type='text'
                                     id='username'
@@ -57,8 +60,11 @@ function Register() {
                                     onChange={(e) => setUsername(e.target.value)}
                                     value={username}
                                     required
+                                    className='form-control'
+                                    minLength={6} 
                                 />
-
+                            </div>
+                            <div  className="mb-3">
                                 <label htmlFor='password'>Contraseña</label>
                                 <input
                                     type='password'
@@ -66,13 +72,19 @@ function Register() {
                                     onChange={(e) => setPassword(e.target.value)}
                                     value={password}
                                     required
+                                    className='form-control'
+                                    minLength={6} 
                                 />
-                                <button className='btn'>Registrarse</button>
+                                </div>
+                                <button  className='btnAzul'>Registrarse</button>
+                                <a href="/login" className='btn-login'>Iniciar sesión</a> 
                             </form>
-                            <button className='btn-login' onClick={() => window.location.pathname='/Login'}>O inicia de sesión</button>
+
+                           
                         </div>
                 </section>
             )}
+    </div>
     </div>
   )
 }
