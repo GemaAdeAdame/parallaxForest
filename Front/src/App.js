ba6d3e335c094ad4fc6329d21cf6779994b3a02e
Marian4gc <mariangarcia1724@gmail.com>
2023-04-12 12:37:44 +0200

    rutas

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Home from './components/Home/Home'
-import Login from './components/login/Login.jsx'
-import Register from './components/registro/register.jsx'
+import Login from './components/login/Login';
+import Register from './components/registro/register';
 import Birds from './components/birds/Birds'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<><Home /> <Birds /> </>
+    },
+    {
+      path:"/Register",
+      element:<><Register />  </>
+    },
+    {
+      path:"/Login",
+      element:<><Login /> </>
+    },
+    
+
+  ]);
+
   return (
-    <div>
-      <Home />
-      <Login />
-      <Register />
-      <Birds />
+    <div className="App">
+          <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
